ff174b0386e5a080c27e1eac5e6b51df37913ea4
maiorBoltach <val.boltach@gmail.com>
2014-10-18 21:18:23 +0300

    Fixed error in dbhost

--- fesl_login_server/_launch.bat
+++ fesl_login_server/_launch.bat
@@ -1,1 +1,1 @@
-start fesl_login_server -v -l 1 -dbhost 192.168.70.206 -dbname test2142 -dbuser us2142 -dbpass pw2142 
+start fesl_login_server -v -l 1 -dbhost 127.0.0.1 -dbname test2142 -dbuser us2142 -dbpass pw2142 
